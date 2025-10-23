3caf7f5795d947f6962dfa3b12ca21ea417fc2cd
Meryem Demir <meryemdmirr0@gmail.com>
2025-10-23 17:05:50 +0300

    Create main screen draft

---
+++ PizzaDelivery/PizzaDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaDeliveryApp.swift
+//  PizzaDelivery
+//
+//  Created by Meryem Demir on 23.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct PizzaDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
